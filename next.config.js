3257555e0e94c8532c73de8fb64a8df660c28d5b
unknown <tamar.shalmon1@gmail.com>
2024-08-06 21:08:44 +0300

    NEXT_PUBLIC_API_URL for deploy

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client"],
+  },
   images:{
     domains: [
       'cdn-icons-png.flaticon.com',
