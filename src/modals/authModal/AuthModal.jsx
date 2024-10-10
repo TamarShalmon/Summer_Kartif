39b4e8cfed83b6c3f657096d93a213fd43fcd9d1
unknown <tamar.shalmon1@gmail.com>
2024-10-10 13:43:42 +0300

    ui authModal

--- src/modals/authModal/AuthModal.jsx
+++ src/modals/authModal/AuthModal.jsx
@@ -1,7 +1,10 @@
-// components/AuthModal.js
 import React from 'react';
 import styles from './AuthModal.module.css';
 import Link from 'next/link';
+
+const handleLogin = () => {
+    onClose(); 
+  };
 
 const AuthModal = ({ onClose }) => {
   return (
@@ -10,7 +13,7 @@ const AuthModal = ({ onClose }) => {
         <button className={styles.closeButton} onClick={onClose}>×</button>
         <h3>?שנתחבר</h3>
         <p className={styles.title} >גם ניכר אותך טוב יותר וגם תיפתח לך גישה מהירה ופשוטה לכל האפשרויות והפיצ'רים שלנו</p>
-        <Link href="/login" className={styles.loginButton}>
+        <Link href="/login" className={styles.loginButton} onClick={handleLogin}>
           להתחברות
         </Link>
       </div>
