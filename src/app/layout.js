2dbd51bea84cd7f759febd21403ec93f867ac6d3
unknown <tamar.shalmon1@gmail.com>
2024-10-15 10:44:40 +0300

    Analytics varcel

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,10 +6,12 @@ import ThemeProvider from "@/providers/ThemeProvider";
 import AuthProvider from "@/providers/AuthProvider";
 import { Fredoka } from 'next/font/google'
 import FooterTamar from "@/components/footerTamar/FooterTamar";
+import { Analytics } from "@vercel/analytics/react"
+
 
 const fredoka = Fredoka({
   subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700'], // Add the weights you need
+  weight: ['300', '400', '500', '600', '700'],
   variable: '--font-fredoka',
 })
 
@@ -23,7 +25,7 @@ export default function RootLayout({ children }) {
     <html lang="he" dir="rtl" className={fredoka.variable}>
       <head>
         <meta property="og:image" content="https://cdn-icons-png.flaticon.com/512/3114/3114886.png" />
-        <link rel="apple-touch-icon" href="https://cdn-icons-png.flaticon.com/512/3114/3114886.png"/>
+        <link rel="apple-touch-icon" href="https://cdn-icons-png.flaticon.com/512/3114/3114886.png" />
       </head>
       <body className={fredoka.variable} >
         <AuthProvider>
@@ -35,6 +37,7 @@ export default function RootLayout({ children }) {
                 </div>
                 <div className="wrapper">
                   {children}
+                  <Analytics />
                 </div>
                 <div className="footer">
                   <Footer />
